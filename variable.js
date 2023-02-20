428af2baf79f9d6a48cb7452ed390c283fd8cfb3
vijaysomwanshi55205 <vijaysomwanshi55205@gmail.com>
2023-02-20 20:22:24 +0530

    variable_assingment
    1st Variable Assingment

---
+++ variable.js
@@ -0,0 +1,33 @@
+var step1="*********** Step 1 ***********";
+console.log(step1);
+var myFullName;
+console.log(myFullName);
+myFullName = "vijay Somwanshi";
+console.log(myFullName);
+myFullName = "vijay Dagadudaseb Somwanshi";
+console.log(myFullName);
+
+var step2="*********** Step 2 ***********";
+console.log(step2);
+var myLove="I Love Only JS";
+console.log(myLove);
+
+var step3="*********** Step 3 ***********";
+console.log(step3);
+var firstName = "First name: Vijay.";
+console.log(firstName);
+var lastName = "Last name: Somwanshi.";
+console.log(lastName);
+var age = "Age: 28.";
+console.log(age);
+var address = "Address: Nanded City. Pune.";
+console.log(address);
+var pincode = "Pin Code:411041";
+console.log(pincode);
+var gender = "Gender: Male";
+console.log(gender);
+var isMarried = "Is Married= No.";
+console.log(isMarried);
+
+
+
